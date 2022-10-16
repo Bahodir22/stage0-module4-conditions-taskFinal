adcb546c69efc199a8e3e20f0b9b2ad604ce6e9c
Bahodir Abdusaxatov <88647942+Bahodir22@users.noreply.github.com>
2022-10-16 10:48:45 +0500

    8
    #

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.finalTask;
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
         if (divider==0){
-            System.out.println("division by zero");
+            System.out.println("/ by zero");
         }
         int b = dividend/divider;
         if (dividend==b*divider){
